edab4faa8412d22b5f93df3207d450648fd86025
Casayr32 <casayrpodcast@gmail.com>
2026-08-26 23:21:48 +0300

    Update app.py

--- ai-service/app.py
+++ ai-service/app.py
@@ -117,5 +117,7 @@ def predict():
         print("❌ ML ERROR:", str(e))
         return jsonify({"error": str(e)}), 400
 
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5001, debug=False)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=False)
+    app.run(host='0.0.0.0', port=5002, debug=False)
